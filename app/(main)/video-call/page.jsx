59e1868e760402cbaab9721c653d877b98571b7e
Priyanshu Paul <priyanshupaul90@gmail.com>
2025-08-13 01:43:36 +0530

    Video Call features

--- app/(main)/video-call/page.jsx
+++ app/(main)/video-call/page.jsx
@@ -4,9 +4,7 @@ import VideoCall from './_components/video-call';
 const VideoCallPage = async({searchParams}) => {
     const {sessionId , token} = await searchParams
 
-  return (
-    <VideoCall sessionId = {sessionId} token = {token} />
-  )
+  return <VideoCall sessionId={sessionId} token={token} />;
 }
 
 export default VideoCallPage;
